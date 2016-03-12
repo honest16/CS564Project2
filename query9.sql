9a4d14a067294c473dff73f57d620eedb575b8ca
Sneha Rudra <sneha@galapagos-07.cs.wisc.edu>
2016-03-12 12:12:11 -0600

    Query9_edit

--- query9.sql
+++ query9.sql
@@ -1,3 +1,4 @@
+
 CREATE OR REPLACE VIEW view1 AS(
 SELECT dept, norm_sales 
 FROM (
@@ -14,12 +15,16 @@ FROM (
 WHERE OrderRank <= 10)
 ;
 
+
 CREATE OR REPLACE VIEW view2 AS(
 SELECT sales.dept as dept, DATE_PART('year', weekdate) AS yr, DATE_PART('month', weekdate) AS month, sales.weeklysales AS weeklysales 
 FROM view1 INNER JOIN sales ON view1.dept = sales.dept);
 
 
+
 CREATE OR REPLACE VIEW view3 AS(SELECT dept, yr, month, sum(weeklysales) AS monthly_sales FROM view2 GROUP BY dept, yr, month ORDER BY dept, yr, month);
 
 SELECT dept, yr, month,SUM(monthly_sales) OVER (PARTITION BY dept ORDER BY dept, yr, month) AS cumulative_sales FROM view3 ORDER BY dept, yr, month;
+
+DROP VIEW view1, view2, view3;
 
