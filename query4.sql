1d6e9234f1c6958c2fca1367c42b85f35da9b7f9
8bitMoose <mmuccianti@gmail.com>
2016-03-11 18:31:47 -0600

    Added files via upload

--- query4.sql
+++ query4.sql
@@ -1,4 +1,17 @@
-SELECT Count(DISTINCT Sales.WeekDate)
-FROM Holidays INNER JOIN Sales ON Sales.WeekDate = Holidays.weekDate
-WHERE Sales.WeeklySales > (SELECT AVG(Sales.WeeklySales) FROM Sales INNER JOIN Holidays ON Sales.WeekDate = Holidays.weekDate WHERE Holidays.IsHoliday = 'True') AND Holidays.IsHoliday = 'False';
-
+SELECT count(*)
+FROM (
+	SELECT sales.weekdate, sum(sales.weeklysales)
+	FROM (
+		Holidays INNER JOIN Sales ON Sales.WeekDate = Holidays.weekDate
+	)
+	WHERE isHoliday = 'False'
+	GROUP BY sales.weekdate
+	HAVING sum(weeklySales) > (	SELECT AVG(sum_sales) 
+								FROM (
+									SELECT SUM(sales.WeeklySales) AS sum_sales
+									FROM (Sales INNER JOIN Holidays ON Sales.WeekDate = Holidays.weekDate) 
+									WHERE holidays.IsHoliday = 'True'
+									GROUP BY sales.weekdate
+								) AS holiday_avg)
+	ORDER BY sales.weekdate
+) AS num_greater;
