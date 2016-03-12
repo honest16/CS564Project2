c1ab898818a327bd8ef10f4f296c308e9bd867e1
Sneha Rudra <sneha@galapagos-07.cs.wisc.edu>
2016-03-12 14:28:40 -0600

    Query7

--- query7.sql
+++ query7.sql
@@ -1,25 +1,29 @@
+
+
 DROP TABLE Output7;
 
 CREATE TABLE Output7(AttributeName VARCHAR(20), CorrSign char(1), CorrValue float);
 
-INSERT INTO Output7 SELECT 'Temperature', (SELECT case when val > 0 then '+'
+
+INSERT INTO Output7 VALUES ('Temperature', (SELECT case when val > 0 then '+'
             else '-' end                                                                                                                                  
-FROM (SELECT corr(Sales.WeeklySales,TemporalData.Temperature) FROM Sales INNER JOIN TemporalData ON Sales.WeekDate = TemporalData.WeekDate) AS float(val)), (SELECT corr(Sales.WeeklySales,TemporalData.Temperature) FROM Sales INNER JOIN TemporalData ON Sales.WeekDate = TemporalData.WeekDate);
+FROM (SELECT corr(TemporalData.Temperature,Sales.WeeklySales) FROM Sales INNER JOIN TemporalData ON Sales.WeekDate = TemporalData.WeekDate AND Sales.Store = TemporalData.Store) as float(val)), (SELECT corr(TemporalData.Temperature,Sales.WeeklySales) FROM Sales INNER JOIN TemporalData ON Sales.WeekDate = TemporalData.WeekDate AND Sales.Store = TemporalData.Store));
 
 
-
-INSERT INTO Output7 SELECT 'Fuel Price', (SELECT case when val > 0 then '+'
+INSERT INTO Output7 VALUES ('Fuel Price', (SELECT case when val > 0 then '+'
             else '-' end                                                                                                                                  
-FROM (SELECT corr(Sales.WeeklySales,TemporalData.FuelPrice) FROM Sales INNER JOIN TemporalData ON Sales.WeekDate = TemporalData.WeekDate) AS float(val)), (SELECT corr(Sales.WeeklySales,TemporalData.FuelPrice) FROM Sales INNER JOIN TemporalData ON Sales.WeekDate = TemporalData.WeekDate);
+FROM (SELECT corr(TemporalData.FuelPrice,Sales.WeeklySales) FROM Sales INNER JOIN TemporalData ON Sales.WeekDate = TemporalData.WeekDate AND Sales.Store = TemporalData.Store) as float(val)), (SELECT corr(TemporalData.FuelPrice,Sales.WeeklySales) FROM Sales INNER JOIN TemporalData ON Sales.WeekDate = TemporalData.WeekDate AND Sales.Store = TemporalData.Store));
 
 
-INSERT INTO Output7 SELECT 'CPI', (SELECT case when val > 0 then '+'
+INSERT INTO Output7 VALUES ('CPI', (SELECT case when val > 0 then '+'
             else '-' end                                                                                                                                  
-FROM (SELECT corr(Sales.WeeklySales,TemporalData.CPI) FROM Sales INNER JOIN TemporalData ON Sales.WeekDate = TemporalData.WeekDate) AS float(val)), (SELECT corr(Sales.WeeklySales,TemporalData.CPI) FROM Sales INNER JOIN TemporalData ON Sales.WeekDate = TemporalData.WeekDate);
+FROM (SELECT corr(TemporalData.CPI,Sales.WeeklySales) FROM Sales INNER JOIN TemporalData ON Sales.WeekDate = TemporalData.WeekDate AND Sales.Store = TemporalData.Store) as float(val)), (SELECT corr(TemporalData.CPI,Sales.WeeklySales) FROM Sales INNER JOIN TemporalData ON Sales.WeekDate = TemporalData.WeekDate AND Sales.Store = TemporalData.Store));
 
 
-INSERT INTO Output7 SELECT 'UnemploymentRate', (SELECT case when val > 0 then '+'
+INSERT INTO Output7 VALUES ('UnemploymentRate', (SELECT case when val > 0 then '+'
             else '-' end                                                                                                                                  
-FROM (SELECT corr(Sales.WeeklySales,TemporalData.UnemploymentRate) FROM Sales INNER JOIN TemporalData ON Sales.WeekDate = TemporalData.WeekDate) AS float(val)), (SELECT corr(Sales.WeeklySales,TemporalData.UnemploymentRate) FROM Sales INNER JOIN TemporalData ON Sales.WeekDate = TemporalData.WeekDate);
+FROM (SELECT corr(TemporalData.UnemploymentRate,Sales.WeeklySales) FROM Sales INNER JOIN TemporalData ON Sales.WeekDate = TemporalData.WeekDate AND Sales.Store = TemporalData.Store) as float(val)), (SELECT corr(TemporalData.UnemploymentRate,Sales.WeeklySales) FROM Sales INNER JOIN TemporalData ON Sales.WeekDate = TemporalData.WeekDate AND Sales.Store = TemporalData.Store));
 
 SELECT * FROM Output7;
+
+DROP TABLE Output7;
